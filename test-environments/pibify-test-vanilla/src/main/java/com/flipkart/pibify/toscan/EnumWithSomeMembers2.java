d5d28e12ca257dc5a3c5b41cff9f4ae76b842fe8
bp <bageshwar.pn@flipkart.com>
2024-10-05 20:33:03 +0530

    Test enum for scanning

---
+++ test-environments/pibify-test-vanilla/src/main/java/com/flipkart/pibify/toscan/EnumWithSomeMembers2.java
@@ -0,0 +1,18 @@
+package com.flipkart.pibify.toscan;
+
+import com.flipkart.pibify.core.Pibify;
+
+/**
+ * This enum is used for
+ * Author bageshwar.pn
+ * Date 05/10/24
+ */
+public enum EnumWithSomeMembers2 {
+
+    @Pibify(1)
+    Value1,
+    @Pibify(2)
+    Value2,
+    Value3,
+    Value4
+}
