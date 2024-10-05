8504b1a02289d3f5a567a1a0bb9055e16983ea71
bp <bageshwar.pn@flipkart.com>
2024-10-05 14:26:01 +0530

    Test source files for scanning

---
+++ test-environments/pibify-test-vanilla/src/main/java/com/flipkart/pibify/toscan/ClassWithNoAnnotation.java
@@ -0,0 +1,13 @@
+package com.flipkart.pibify.toscan;
+
+/**
+ * This class is used for
+ * Author bageshwar.pn
+ * Date 23/09/24
+ */
+public class ClassWithNoAnnotation {
+    public String publicString;
+    protected String protectedString;
+    private String privateString;
+    private String multiLine1, multiLine2;
+}
