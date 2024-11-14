8d7284b26ab0cb7f32b31e5353599e7e4959b1b5
bp <bageshwar.pn@flipkart.com>
2024-11-14 13:50:22 +0530

    Fetch the raw backing bytearray of string for serialization. Improved ~10% performance.

--- pibify-core/src/main/java/com/flipkart/pibify/serde/PibifySerializer.java
+++ pibify-core/src/main/java/com/flipkart/pibify/serde/PibifySerializer.java
@@ -7,8 +7,8 @@ import com.google.protobuf.WireFormat;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
-
 /**
  * This class serves as the base serializer that the generated code uses.
  * Author bageshwar.pn
@@ -18,6 +18,36 @@ public class PibifySerializer extends BaseSerde implements ISerializer {
 
     private final CodedOutputStream codedOutputStream;
     private final ByteArrayOutputStream outputStream;
+
+    // effectively final
+    private static Field declaredStringValueField;
+    private final static boolean useUnsafeStringOperation = computeUseUnsafeStringOperation();
+
+    private static boolean computeUseUnsafeStringOperation() {
+        try {
+            String value = "unsafeStringOps";
+            byte[] bytesFromString = getUnsafeBytesFromString(value);
+            return value.equals(new String(bytesFromString));
+        } catch (Exception e) {
+            // cheap logging, since this is platform level.
+            // --add-opens java.base/java.lang=ALL-UNNAMED
+            e.printStackTrace();
+            // might revisit later
+            System.err.println("[Pibify] Won't be using Unsafe String operations");
+            return false;
+        }
+    }
+
+    private static byte[] getUnsafeBytesFromString(String value) throws Exception {
+        if (declaredStringValueField == null) {
+            synchronized (PibifySerializer.class) {
+                declaredStringValueField = String.class.getDeclaredField("value");
+                declaredStringValueField.setAccessible(true);
+            }
+        }
+
+        return (byte[]) declaredStringValueField.get(value);
+    }
 
     public PibifySerializer(int size) {
         // TODO use pre-computed estimates on the size of the buffer
@@ -130,6 +160,16 @@ public class PibifySerializer extends BaseSerde implements ISerializer {
     @Override
     public void writeString(int index, String value) throws IOException {
         if (value != null) {
+            if (useUnsafeStringOperation) {
+                try {
+                    codedOutputStream.writeByteArray(index, getUnsafeBytesFromString(value));
+                    return;
+                } catch (Exception e) {
+                    // consume exception and try traditionally
+                    // TODO remember this error and maybe permanently revert to using safe operations
+                }
+            }
+
             codedOutputStream.writeByteArray(index, value.getBytes(StandardCharsets.UTF_8));
         }
     }
@@ -152,7 +192,6 @@ public class PibifySerializer extends BaseSerde implements ISerializer {
     public void writeObject(int index, PibifyGenerated handler, Object value) throws PibifyCodeExecException, IOException {
         if (value != null) {
             codedOutputStream.writeTag(index, WireFormat.WIRETYPE_LENGTH_DELIMITED);
-            ;
             handler.serialize(value, this);
             codedOutputStream.writeTag(1, WireFormat.WIRETYPE_END_GROUP);
         }
