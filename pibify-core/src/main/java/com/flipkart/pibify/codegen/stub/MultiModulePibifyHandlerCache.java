6b3874f8ece5d1be860e7fa8b97424fc78d52d52
bp <bageshwar.pn@flipkart.com>
2025-01-06 23:08:12 +0530

    Minor fix to multi-module

--- pibify-core/src/main/java/com/flipkart/pibify/codegen/stub/MultiModulePibifyHandlerCache.java
+++ pibify-core/src/main/java/com/flipkart/pibify/codegen/stub/MultiModulePibifyHandlerCache.java
@@ -8,6 +8,11 @@ package com.flipkart.pibify.codegen.stub;
 public class MultiModulePibifyHandlerCache extends AbstractPibifyHandlerCache {
 
     public MultiModulePibifyHandlerCache(AbstractPibifyHandlerCache... caches) {
+
+        // TODO
+        // remove dependency on concrete cache in handler and use abstract cache that is passed in constructor
+        // multimodule should rewire the handler to use the multimodule abstract cache
+
         // When this method is called, the concrete instance of cache are available
         // as per static block by this time, the cache has been backed and the handlers have been initialized
         // Hence just consolidating the maps here.
